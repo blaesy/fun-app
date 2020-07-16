7c31f60f9099ba61a5137705d0ceea581d025d7e
Patryk Wietrak <patrickwietrak@gmail.com>
2020-07-16 19:15:38 +0100

    Updates

---
+++ precache-manifest.caa050b9792775eab4e0a70fc8910de9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a768bae34e694279a0236f03b6f06da0",
+    "url": "/fun-app/index.html"
+  },
+  {
+    "revision": "fabd75b0daf6c8b5d66f",
+    "url": "/fun-app/static/css/main.e9cd4286.chunk.css"
+  },
+  {
+    "revision": "addf9eb9577838b78d0d",
+    "url": "/fun-app/static/js/2.fc49b6f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fun-app/static/js/2.fc49b6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabd75b0daf6c8b5d66f",
+    "url": "/fun-app/static/js/main.0877ab60.chunk.js"
+  },
+  {
+    "revision": "7bf916e4c731e59851c7",
+    "url": "/fun-app/static/js/runtime-main.cdb73f8f.js"
+  },
+  {
+    "revision": "9c40eec372bf98cc307b56928fc5ee07",
+    "url": "/fun-app/static/media/XDDD.9c40eec3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fun-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
